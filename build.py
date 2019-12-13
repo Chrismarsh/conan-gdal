3c1fcf4882607f129eb98895860db602e70ac5ce
Chris Marsh <chrismarsh.c2@gmail.com>
2019-12-13 15:33:48 -0600

    updated travis ci

--- build.py
+++ build.py
@@ -1,13 +1,26 @@
-
-from conan.packager import ConanMultiPackager
-
-def main():
-    """ Main function """
-
-    builder = ConanMultiPackager()
-    builder.add_common_builds()
-    builder.remove_build_if(lambda build: build.settings["arch"] != "x86_64")
-    builder.run()
+from cpt.packager import ConanMultiPackager
+from collections import defaultdict
 
 if __name__ == "__main__":
-    main()
+    builder = ConanMultiPackager(
+                                archs=["x86_64"],
+                                build_types=["Release"])
+                              
+    builder.add_common_builds(pure_c=False,shared_option_name="gdal:shared")
+
+    builder.remove_build_if(lambda build: build.settings["compiler.libcxx"] == "libstdc++")
+
+    named_builds = defaultdict(list)
+    for settings, options, env_vars, build_requires, reference in builder.items:
+
+        shared="shared"
+
+        if not options['gdal:shared']:
+            shared = "static" 
+
+        named_builds[settings['compiler'] +"_"+shared].append([settings, options, env_vars, build_requires, reference])
+
+    builder.named_builds = named_builds
+
+    builder.run()
+
