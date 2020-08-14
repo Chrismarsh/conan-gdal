c29a981fe88981aecd956620b6d0c9630e34a5e1
Chris Marsh <chrismarsh.c2@gmail.com>
2020-08-14 11:40:49 -0600

    only test all enabled options

--- build.py
+++ build.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
                                 archs=["x86_64"],
                                 build_types=["Release"])
                               
-    builder.add_common_builds(pure_c=False,shared_option_name="gdal:shared", build_all_options_values=["gdal:libcurl", "gdal:netcdf"])
+    builder.add_common_builds(pure_c=False,shared_option_name="gdal:shared") #, build_all_options_values=["gdal:libcurl", "gdal:netcdf"])
 
     builder.remove_build_if(lambda build: build.settings["compiler.libcxx"] == "libstdc++")
 
@@ -18,9 +18,11 @@ if __name__ == "__main__":
         if not options['gdal:shared']:
             shared = "static" 
 
-        name = f'{settings['compiler']}_{shared}_libcurl-{options['gdal:libcurl']}_netcdf-{options['gdal:netcdf']}'
+        options["gdal:libcurl"]=True
+        options["gdal:netcdf"]=True
+        # name = f'{settings['compiler']}_{shared}_libcurl-{options['gdal:libcurl']}_netcdf-{options['gdal:netcdf']}'
 
-        named_builds[name].append([settings, options, env_vars, build_requires, reference])
+        named_builds[settings['compiler']+f'_{shared}'].append([settings, options, env_vars, build_requires, reference])
 
     builder.named_builds = named_builds
 
